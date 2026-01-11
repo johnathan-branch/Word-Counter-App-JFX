ef9128314b9be639bd18b00ebbc690096fcb913f
Johnathan Branch <johnathan.branch@ymail.com>
2026-01-10 20:40:31 -0800

    Fix bug added in 1.1.2b that failed to load the app icon

--- src/WordCounterJFX/utils/GetIconPath.java
+++ src/WordCounterJFX/utils/GetIconPath.java
@@ -5,8 +5,14 @@ import java.io.File;
 public class GetIconPath {
 
     public static String getIconPath() {
-        String invocationDir = System.getProperty("user.dir");
-        String pathToIcon = invocationDir + "\\bin\\WordCounterJFX\\resources\\icon.png";
+        //String invocationDir = System.getProperty("user.dir");
+        //String pathToIcon = invocationDir + "\\bin\\WordCounterJFX\\resources\\icon.png";
+        File invocationDir = new File(System.getProperty("user.dir"));
+        File parentDir = invocationDir.getParentFile();
+
+        String parentDirStr = parentDir.getAbsolutePath();
+        String pathToIcon = parentDirStr + "\\bin\\WordCounterJFX\\resources\\icon.png";
+
         return new File(pathToIcon).toURI().toString();
     }
 }
